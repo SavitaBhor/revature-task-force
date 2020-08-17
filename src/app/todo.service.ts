d63715db28a89738925d87031db16b487e921a08
SavitaBhor <savita.bhor@gmail.com>
2020-08-17 14:05:31 -0400

    Added basic structure and components

---
+++ src/app/todo.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Todo } from './Todo';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  todosUrl:string = 'https://jsonplaceholder.typicode.com/todos';
+  //todosUrl:string = 'http://100.25.22.116:8080/todos';
+
+  constructor(private http:HttpClient) { }
+
+  // Get Todos
+  getTodos():Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.todosUrl}`);
+    //return this.http.get<Todo[]>(`${this.todosUrl}`);
+  }
+
+  // Toggle Completed
+  toggleCompleted(todo: Todo):Observable<any> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.put(url, todo, httpOptions);
+  }
+  
+  // Delete Todo
+  deleteTodo(todo:Todo):Observable<Todo> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.delete<Todo>(url, httpOptions);
+  }
+
+  // Add Todo
+  addTodo(todo:Todo):Observable<Todo> {
+    return this.http.post<Todo>(this.todosUrl, todo, httpOptions);
+  }
+
+}
